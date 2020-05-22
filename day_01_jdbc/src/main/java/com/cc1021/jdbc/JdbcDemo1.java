e75795930fa0427eb483c8567b3b092839255383
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-22 16:30:34 +0800

    数据库警告去除

--- day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
+++ day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
@@ -4,11 +4,11 @@ import java.sql.*;
 
 public class JdbcDemo1 {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         //1、注册驱动
         DriverManager.registerDriver(new com.mysql.jdbc.Driver());
         //2、获取链接
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1?useSSL=false", "root", "");
         //3、获取操作数据库的预处理对象
         PreparedStatement pstm = conn.prepareStatement("select * from student");
         //4、执行sql，得到结果集
