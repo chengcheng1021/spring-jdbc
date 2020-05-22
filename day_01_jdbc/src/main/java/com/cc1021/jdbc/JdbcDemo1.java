4abcc9be282fd4dd59e0d53a21f078a15b59311a
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-22 17:13:06 +0800

    第一个小例子完成

--- day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
+++ day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
@@ -6,7 +6,8 @@ public class JdbcDemo1 {
 
     public static void main(String[] args) throws Exception {
         //1、注册驱动
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Class.forName("com.mysql.jdbc.Driver");
         //2、获取链接
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1?useSSL=false", "root", "");
         //3、获取操作数据库的预处理对象
