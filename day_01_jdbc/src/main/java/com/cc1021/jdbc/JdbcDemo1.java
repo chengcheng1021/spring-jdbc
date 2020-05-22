700913e0fa5bde3cfa5c8e45f268b8bd4c54d6d7
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-22 16:19:07 +0800

    连接数据库

--- day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
+++ day_01_jdbc/src/main/java/com/cc1021/jdbc/JdbcDemo1.java
@@ -1,9 +1,25 @@
 package com.cc1021.jdbc;
 
+import java.sql.*;
+
 public class JdbcDemo1 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
         //1、注册驱动
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
         //2、获取链接
-        //3、
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "");
+        //3、获取操作数据库的预处理对象
+        PreparedStatement pstm = conn.prepareStatement("select * from student");
+        //4、执行sql，得到结果集
+        ResultSet rs = pstm.executeQuery();
+        //5、遍历结果集
+        while (rs.next()){
+            System.out.println(rs.getString("name"));
+        }
+        //6、释放资源
+        rs.close();
+        pstm.close();
+        conn.close();
     }
 }
