ab92ce19fa5fcb19e393ab4e5ed0ec0d8136f14a
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-23 11:00:56 +0800

    简单工厂模式

--- day_02_factory/src/main/java/com/cc1021/ui/Client.java
+++ day_02_factory/src/main/java/com/cc1021/ui/Client.java
@@ -13,7 +13,7 @@ public class Client {
         //IAccountService as = new AccountServiceImpl();
         for(int i=0; i<5; i++){
             IAccountService as = (IAccountService) BeanFactory.getBean("accountService");
-            //System.out.println(as);
+            System.out.println(as);
             as.saveAccount();
         }
 
