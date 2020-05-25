45cac7687d624d5bd32f43c051562547df5503af
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-25 16:15:37 +0800

    set方法注入

---
+++ day_05_DI/src/main/java/com/cc1021/service/impl/AccountServiceImpl2.java
@@ -0,0 +1,28 @@
+package com.cc1021.service.impl;
+
+import com.cc1021.service.IAccountService;
+
+import java.util.Date;
+
+public class AccountServiceImpl2 implements IAccountService {
+    //如果是经常变化的数据，并不适合用于注入的方式
+    private String name;
+    private Integer age;
+    private Date birthday;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void saveAccount() {
+        System.out.println("service 中的 saveAccount 执行了。。。" + name + "," + age + "," + birthday);
+    }
+}
