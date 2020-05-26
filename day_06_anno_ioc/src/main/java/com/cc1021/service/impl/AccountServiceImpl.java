0ca3178e2cf04d428fbfeaffe8ff58fabd14f883
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-26 11:15:21 +0800

    注入数据的注解

--- day_06_anno_ioc/src/main/java/com/cc1021/service/impl/AccountServiceImpl.java
+++ day_06_anno_ioc/src/main/java/com/cc1021/service/impl/AccountServiceImpl.java
@@ -4,7 +4,10 @@ import com.cc1021.service.IAccountService;
 import com.cc1021.dao.IAccountDao;
 import com.cc1021.dao.impl.AccountDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  *  账户的业务层实现类
@@ -30,13 +33,31 @@ import org.springframework.stereotype.Component;
  *
  *  用于注入数据的
  *      他们的作用和在xml配置文件中编写一个<bean>标签中写一个<property></property>实现的功能是一样的
- *      Autowired：
+ *      @Autowired：
  *          作用：自动按照类型注入。只要容器中有唯一的一个bean对象类型和要注入的的变量类型匹配，就可以注入成功
  *              如果ioc容器中没有任何bean的类型和要注入的变量类型匹配，则报错。
  *          出现位置：
  *              可以是变量上，也可以是方法上
  *          细节：
  *              在使用注解注入时，set方法就不是必须的了。
+ *      @Qualifier：
+ *          作用：在按照类中注入的基础之上再按照名称注入。它在给类成员注入时不能单独使用。但是在给方法参数注入时可以
+ *          属性：
+ *              value：用于指定注入bean的id。
+ *
+ *      @Resource
+ *          作用：直接按照bean的id注入。它可以独立使用
+ *          属性：
+ *              name：用于指定bean的id。
+ *
+ *       以上三个注入都只能注入其他bean类型的数据，而基本类型和String类型无法使用上述注解实现。
+ *       另外，集合类型的注入只能通过xml来实现。
+ *
+ *      @Value
+ *          作用：用于注入基本类型和String类型的注入
+ *          属性：
+ *              value：用于指定数据的值。它可以使用spring中的SpEL（也就是spring的el表达式）
+ *                  SpEL的写法：${表达式}
  *
  *
  *  用于改变作用范围的
@@ -47,7 +68,9 @@ import org.springframework.stereotype.Component;
 @Component("accountService")
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired
+//    @Autowired
+//    @Qualifier("accountDao")
+    @Resource(name = "accountDao")
     private IAccountDao accountDao2;
 
     public AccountServiceImpl() {
