83d4025caa44fb59e5c1263e40a9324d600134b5
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-27 20:26:15 +0800

    spring整合Junit

--- day_09_account_annoioc_withoutxml/src/test/java/test/AccountServiceTest.java
+++ day_09_account_annoioc_withoutxml/src/test/java/test/AccountServiceTest.java
@@ -3,24 +3,39 @@ package test;
 import com.cc1021.domain.Account;
 import com.cc1021.service.IAccountService;
 import config.SpringConfiguration;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 /**
  *  使用Junit单元测试，测试我们的配置
+ *  Spring 整合 junit 的配置
+ *      1、导入 Spring 整合 junit 的 jar（坐标）
+ *      2、使用 Junit 提供的一个注解把原有的 main 方法替换了，替换成 spring 提供的
+ *          @RunWith
+ *      3、告知 spring 的运行器，spring 和 ioc 创建是基于 xml 还是注解的，并且说明位置
+ *          @ContextConfiguration
+ *              locations：指定 xml 文件的位置，加上 classpath 关键字，表示在类路径下
+ *              classes：指定注解类所在的位置
+ *      备注：当我们使用 spring 5.x版本的时候，要求 junit 的 jar 必须是4.12及以上
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService as = null;
 
     @Test
     public void testFindAll() {
-        //1、获取容器
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        //2、得到业务层对象
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         //3、执行方法
         List<Account> accounts = as.findAllAccount();
         for (Account account : accounts){
@@ -30,10 +45,6 @@ public class AccountServiceTest {
 
     @Test
     public void testFindOne() {
-        //1、获取容器
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        //2、得到业务层对象
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         //3、执行方法
         Account account = as.findAccountById(1);
         System.out.println(account);
@@ -44,20 +55,13 @@ public class AccountServiceTest {
         Account account = new Account();
         account.setName("test");
         account.setMoney(12345f);
-        //1、获取容器
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        //2、得到业务层对象
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
+
         //3、执行方法
         as.saveAccount(account);
     }
 
     @Test
     public void testUpdate() {
-        //1、获取容器
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        //2、得到业务层对象
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         //3、执行方法
         Account account = as.findAccountById(4);
         account.setMoney(23456f);
@@ -66,10 +70,6 @@ public class AccountServiceTest {
 
     @Test
     public void testDelete() {
-        //1、获取容器
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        //2、得到业务层对象
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         //3、执行方法
         as.deleteAccount(4);
     }
