05490f63499b888a098d77d413a7ef0828978534
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-29 19:24:13 +0800

    基于接口的动态代理

---
+++ day_11_proxy/src/main/java/com/cc1021/proxy/IProducer.java
@@ -0,0 +1,16 @@
+package com.cc1021.proxy;
+
+public interface IProducer {
+
+    /**
+     * 销售
+     * @param money
+     */
+    public void saleProduct(float money);
+
+    /**
+     * 售后
+     * @param money
+     */
+    public void afterService(float money);
+}
