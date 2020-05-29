fdbcfd5692f0c0543cdbc33fe2bd1ad99a9d5413
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-29 15:00:52 +0800

    暂时告一段落

---
+++ day_10_account/src/main/java/com/cc1021/service/impl/AccountServiceImpl_old.java
@@ -0,0 +1,154 @@
+package com.cc1021.service.impl;
+
+import com.cc1021.dao.IAccountDao;
+import com.cc1021.domain.Account;
+import com.cc1021.service.IAccountService;
+import com.cc1021.utils.TransactionManager;
+
+import java.util.List;
+
+/**
+ *  账户的业务层实现类
+ *
+ *  事物控制应该都在业务层
+ */
+public class AccountServiceImpl_old implements IAccountService {
+
+    private IAccountDao accountDao;
+    private TransactionManager txManager;
+
+    public void setTxManager(TransactionManager txManager) {
+        this.txManager = txManager;
+    }
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public List<Account> findAllAccount() {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+            //2、执行操作
+            List<Account> accounts = accountDao.findAllAccount();
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+            return accounts;
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+
+    public Account findAccountById(Integer accountId) {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+            //2、执行操作
+            Account account = accountDao.findAccountById(accountId);
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+            return account;
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+
+    public void saveAccount(Account account) {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+            //2、执行操作
+            accountDao.saveAccount(account);
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+
+    public void updateAccount(Account account) {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+            //2、执行操作
+            accountDao.updateAccount(account);
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+
+    public void deleteAccount(Integer accountId) {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+            //2、执行操作
+            accountDao.deleteAccount(accountId);
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+
+    public void transfer(String sourceName, String targetName, Float money) {
+        try {
+            //1、开启事务
+            txManager.beginTransaction();
+
+            //2、执行操作
+            //2.1、根据名称查询转出账户
+            Account source = accountDao.findAccountByName(sourceName);
+            //2.2、根据名称查询转入账户
+            Account target = accountDao.findAccountByName(targetName);
+            //2.3、转出账户减钱
+            source.setMoney(source.getMoney()-money);
+            //int i = 100/0;
+            //2.4、转入账户加钱
+            target.setMoney(target.getMoney()+money);
+            //2.5、更新转出账户
+            accountDao.updateAccount(source);
+            //2.6、更新转入账户
+            accountDao.updateAccount(target);
+
+            //3、提交事务
+            txManager.commit();
+            //4、返回结果
+        } catch (Exception e) {
+            //5、回滚操作
+            txManager.rollback();
+            e.printStackTrace();
+        } finally {
+            //6、释放连接
+            txManager.release();
+        }
+    }
+}
