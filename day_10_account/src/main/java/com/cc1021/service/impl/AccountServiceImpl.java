bc9286057ac54eb1920276694e145a8504d81c03
chengcheng1021 <chengcheng1021@aliyun.com>
2020-05-29 20:25:26 +0800

    动态代理控制事务

--- day_10_account/src/main/java/com/cc1021/service/impl/AccountServiceImpl.java
+++ day_10_account/src/main/java/com/cc1021/service/impl/AccountServiceImpl.java
@@ -15,11 +15,6 @@ import java.util.List;
 public class AccountServiceImpl implements IAccountService {
 
     private IAccountDao accountDao;
-    private TransactionManager txManager;
-
-    public void setTxManager(TransactionManager txManager) {
-        this.txManager = txManager;
-    }
 
     public void setAccountDao(IAccountDao accountDao) {
         this.accountDao = accountDao;
@@ -46,7 +41,7 @@ public class AccountServiceImpl implements IAccountService {
     }
 
     public void transfer(String sourceName, String targetName, Float money) {
-        //2、执行操作
+        System.out.println("transfer......");
         //2.1、根据名称查询转出账户
         Account source = accountDao.findAccountByName(sourceName);
         //2.2、根据名称查询转入账户
@@ -57,7 +52,7 @@ public class AccountServiceImpl implements IAccountService {
         target.setMoney(target.getMoney()+money);
         //2.5、更新转出账户
         accountDao.updateAccount(source);
-        int i = 100/0;
+        //int i = 100/0;
         //2.6、更新转入账户
         accountDao.updateAccount(target);
     }
