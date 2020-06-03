7156ff5f486eedb939c93bbd8baf79a726a0db41
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-03 16:53:26 +0800

    代码初始化

---
+++ day_15anno_tx_withoutxml/src/main/java/com/cc1021/jdbctemplate/JdbcTemplateDemo4.java
@@ -0,0 +1,30 @@
+package com.cc1021.jdbctemplate;
+
+import com.cc1021.dao.IAccountDao;
+import com.cc1021.domain.Account;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *  JdbcTemplate 的 CRUD 操作
+ */
+public class JdbcTemplateDemo4 {
+
+    public static void main(String[] args) {
+        //1、获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2、获取对象
+        IAccountDao accountDao = ac.getBean("accountDao", IAccountDao.class);
+
+        Account account = accountDao.findAccountById(1);
+        System.out.println(account);
+
+        account.setMoney(30000f);
+        accountDao.updateAccount(account);
+    }
+}
